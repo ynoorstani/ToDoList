813245e6afc4c16a6e193c786ca0cfde421996be
Yama Noorstani <ynoorstani@gmail.com>
2016-01-10 17:08:25 -0500

    added fireBase and made changes to uirouter

--- js/app.js
+++ js/app.js
@@ -1,9 +1,77 @@
 var app = angular.module("sampleApp", ["firebase"]);
 
 
-app.controller("SampleCtrl", function($scope, $firebaseArray) {
-  var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/messages");
-  // create a synchronized array
-  // click on `index.html` above to see it used in the DOM!
-  $scope.messages = $firebaseArray(ref);
+
+app.controller("SampleCtrl", function($scope, $firebaseArray){
+
+
+
+	var ref = new Firebase("https://torrid-fire-2602.firebaseio.com");
+	
+	
+	// create a sychronized array
+	 $scope.messages = $firebaseArray(ref);
+	
+
+	// add new items to the array
+	// the message is automatically added to your Firebase database!
+	$scope.addMessage = function(){
+		$scope.messages.$add({
+			text: $scope.newMessageText
+		});
+	};
+
+	$scope.completeTask = function(msg){
+		console.log($scope.messages);
+		msg.completed = true;
+
+		$scope.messages.$save(2).then(function(ref) {
+		  ref.key() === $scope.messages[2].$id; // true
+		});
+
+		console.log($scope.messages);
+	}
+	// click on index.html above
+	// to see $remove() and $save() in action
+	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
